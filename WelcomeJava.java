26e775d7769634eb1eae6568d56e6666e13bfd62
Tapas Bhattacharya <38348932+tbtapascse@users.noreply.github.com>
2020-02-05 18:13:33 +0530

    Commit 2

--- WelcomeJava.java
+++ WelcomeJava.java
@@ -4,7 +4,7 @@ public class WelcomeJava {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		for(int i=0; i<10;i++) {
+		for(int i=0; i<15;i++) {
 			
 			System.out.println("Welcome to Java");
 			Thread.sleep(2000);
